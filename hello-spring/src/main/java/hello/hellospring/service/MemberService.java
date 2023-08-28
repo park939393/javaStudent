ec84225180e52051457f113029607e6cef840849
park939393 <smdqkr0490@naver.com>
2023-08-28 23:51:54 +0900

    0828 스프링부트 도메인 설계 공부

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -6,11 +6,13 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -21,14 +23,14 @@ public class MemberService {
     }
 
     /*회원가입 */
+
     public Long join(Member member){
 
-        // 같은  이름의 중복 회원 x
-        valitateDuplicateMember(member);
-        memberRepository.save(member);
-
-        return  member.getId();
-    }
+            // 같은  이름의 중복 회원 x
+            valitateDuplicateMember(member);
+            memberRepository.save(member);
+            return  member.getId();
+        }
 
 
     private void valitateDuplicateMember(Member member) {
